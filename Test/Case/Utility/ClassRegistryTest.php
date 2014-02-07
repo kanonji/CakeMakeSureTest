14bfa6a86bfa56dcaba6e1f626af229586d122e7
kanonji <kanonji@example.com>
2014-02-07 18:15:59 +0900

    Barebone test case

---
+++ Test/Case/Utility/ClassRegistryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * ClassRegistry Test Case
+ *
+ */
+class ClassRegistryTest extends CakeTestCase {
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		// Do something
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		// Do something
+		parent::tearDown();
+	}
+
+}
